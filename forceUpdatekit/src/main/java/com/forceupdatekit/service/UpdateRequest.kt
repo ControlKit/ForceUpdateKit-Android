b3023e5f6f4f0beb05306353640de083c717648a
MortezaSakiyan <mortezasakianm16@gmail.com>
2025-04-25 20:07:34 +0330

    delete

--- forceUpdatekit/src/main/java/com/forceupdatekit/service/UpdateRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.forceupdatekit.service
-
-data class UpdateRequest(  var appId: String? = null,
- var version: String ,
- var route: String,
- var os: String = "Android",
- var language: String = "en")
--- forceUpdatekit/src/main/java/com/forceupdatekit/view/config/ForceUpdateViewContract.kt
+++
@@ -1,9 +0,0 @@
-package com.forceupdatekit.view.config
-
-import androidx.compose.runtime.Composable
-import com.example.demoapp.models.CheckUpdateResponse
-
-interface ForceUpdateViewContract {
-    @Composable
-    fun ShowView(config: ForceUpdateViewConfig,response: CheckUpdateResponse)
-}
